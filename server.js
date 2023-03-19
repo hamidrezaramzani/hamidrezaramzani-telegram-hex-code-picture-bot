883240af47f3678d4e04573c2f1e88a493d7abf2
hamidrezaramzani <hamidrezaramzani80@gmail.com>
2023-03-19 13:13:16 +0330

    initial bot project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const expressApp = express();
+const axios = require("axios");
+const path = require("path");
+const port = process.env.PORT || 3000;
+expressApp.use(express.static("static"));
+expressApp.use(express.json());
+require("dotenv").config();
+
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.command("start", (ctx) => {
+  console.log(ctx.from);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "Hello there! Welcome to the Code Capsules telegram bot.\nI respond to /ethereum. Please try it",
+    {}
+  );
+});
+
+bot.launch();

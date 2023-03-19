bf8c5f8e11a27f7207e17ee10161f11477479b9a
hamidrezaramzani <hamidrezaramzani80@gmail.com>
2023-03-19 15:27:16 +0330

    export module

--- server.js
+++ server.js
@@ -5,42 +5,40 @@ expressApp.use(express.json());
 require("dotenv").config();
 
 const { Telegraf } = require("telegraf");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-module.exports = () => {
-  const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.command("start", (ctx) => {
+  console.log(ctx.from);
+  bot.telegram.sendMessage(
+    ctx.chat.id,
+    "سلام! به ربات تصویر رنگ هکس کد خوش اومدی. اینجا یه هکس کد بده رنگشو بهت بدم!",
+    {}
+  );
+});
 
-  bot.command("start", (ctx) => {
-    console.log(ctx.from);
+bot.on("text", async (ctx) => {
+  const hexCodeRegex = /^#(?:[0-9a-fA-F]{6}){1,2}$/;
+  if (!hexCodeRegex.test(ctx.message.text.trim())) {
     bot.telegram.sendMessage(
       ctx.chat.id,
-      "سلام! به ربات تصویر رنگ هکس کد خوش اومدی. اینجا یه هکس کد بده رنگشو بهت بدم!",
+      "لطفا یک رنگ کد معتبر وارد کنید ",
       {}
     );
-  });
+  } else {
+    await bot.telegram.sendPhoto(
+      ctx.chat.id,
+      {
+        url: `https://singlecolorimage.com/get/${ctx.message.text.replace(
+          "#",
+          ""
+        )}/800x800`,
+      },
+      {
+        caption: ctx.message,
+      }
+    );
+  }
+});
 
-  bot.on("text", async (ctx) => {
-    const hexCodeRegex = /^#(?:[0-9a-fA-F]{6}){1,2}$/;
-    if (!hexCodeRegex.test(ctx.message.text.trim())) {
-      bot.telegram.sendMessage(
-        ctx.chat.id,
-        "لطفا یک رنگ کد معتبر وارد کنید ",
-        {}
-      );
-    } else {
-      await bot.telegram.sendPhoto(
-        ctx.chat.id,
-        {
-          url: `https://singlecolorimage.com/get/${ctx.message.text.replace(
-            "#",
-            ""
-          )}/800x800`,
-        },
-        {
-          caption: ctx.message,
-        }
-      );
-    }
-  });
-
-  bot.launch();
-};
+bot.launch();
+module.exports = () => {};
